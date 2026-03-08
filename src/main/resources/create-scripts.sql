7500281afbd74d9dedcee8e8ebd9ca5bb6e15b06
roberto <roberto.bani@gmail.com>
2026-03-08 18:44:43 +0100

    modified uniques indexes,

--- src/main/resources/create-scripts.sql
+++ src/main/resources/create-scripts.sql
@@ -6,9 +6,29 @@ CREATE TABLE public.authors
     created_at timestamp without time zone NOT NULL DEFAULT current_timestamp,
     updated_at timestamp without time zone,
     deleted_at timestamp without time zone,
-    CONSTRAINT pk_authors PRIMARY KEY (id),
-    CONSTRAINT unique_authors_first_name_last_name UNIQUE (first_name, last_name)
+    CONSTRAINT pk_authors PRIMARY KEY (id)
 );
 
+CREATE UNIQUE INDEX unique_authors_first_name_last_name
+ON authors (first_name, last_name)
+WHERE deleted_at IS NULL;
+
 ALTER TABLE IF EXISTS public.authors
+    OWNER to books_archive;
+
+CREATE TABLE public.publishers
+(
+    id bigserial NOT NULL, 
+    name character varying(255),
+    created_at timestamp without time zone NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp without time zone,
+    deleted_at timestamp without time zone,
+    CONSTRAINT pk_publishers PRIMARY KEY (id),
+);
+
+CREATE UNIQUE INDEX unique_publishers_name
+ON publishers (name)
+WHERE deleted_at IS NULL;
+
+ALTER TABLE IF EXISTS public.publishers
     OWNER to books_archive;
